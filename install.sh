d06f906a06f9abf3b8f6d48f94cd2e6dcea848d7
Julio Torres <jtorres990@gmail.com>
2026-01-04 13:56:15 -0500

    refactor(install): enhance script structure and add missing commands

--- install.sh
+++ install.sh
@@ -1,30 +1,45 @@
 #!/bin/sh
 
+# Variables
 app_name=doppler
 bin_path=/tmp/$app_name-6415415
 
+# Check for root privileges
 if [ $USER != "root" ]; then
   echo "This installer must be run as root."
   exit 1
 fi
 
+# Build application
 mkdir -p bin
 rm -rf bin/*
 GOOS=linux GOARCH=amd64 go build -o $bin_path
 
+# Install application
 systemctl stop $app_name > /dev/null
 cp $bin_path /usr/bin/$app_name
 mkdir -p /etc/$app_name
 
+# Configure application
+if [ ! -f config.yml ]; then
+  cp config.yml.example config.yml
+fi
 cp config.yml /etc/$app_name &> /dev/null
 cp config.yml.example /etc/$app_name &> /dev/null
 cp $app_name.service /etc/systemd/system
+
+# Create system user
 adduser $app_name \
     --gecos "" \
     --system \
     --no-create-home \
     --disabled-password \
     --disabled-login > /dev/null
+
+# Set permissions
+chown -R $app_name:$app_name /etc/$app_name
+
+# Start application
 systemctl daemon-reload
 systemctl start $app_name
 systemctl status $app_name
